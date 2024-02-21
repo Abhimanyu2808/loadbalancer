b803de3f6ef3bc1754d86d5a6a851bf1948e40bf
Abhimanyu2808 <abhimanyupatil2808@gmail.com>
2024-02-21 22:58:32 +0530

    add

--- modules/autoscaling/output.tf
+++ modules/autoscaling/output.tf
@@ -1,11 +1,11 @@
-output = "as-home-group-name" {
+output "as-home-group-name" {
      value = aws_autoscaling_group.as-home-gp.name
 } 
 
-output = "as-laptop-group-name" {
+output "as-laptop-group-name" {
      value = aws_autoscaling_group.as-laptop-gp.name
 } 
 
-output = "as-mobile-group-name" {
+output "as-mobile-group-name" {
      value = aws_autoscaling_group.as-mobile-gp.name
 } 
